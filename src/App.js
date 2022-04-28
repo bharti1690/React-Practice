08d99a36eb97f1e183f94bae8b744116a793e142
Bharti Sharma <sharma.bharti17@yahoo.com>
2022-04-28 01:37:11 -0400

    image-slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { SearchInput } from './search-input';
+import { SearchInput } from './search-input/search-input';
 import { useState } from 'react';
+import Slider from './image-slider/slider';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
 
   const [isPreviewShown,setIsPreviewShown] = useState(false)
+  const [isSlider,setIsSlider] = useState(false)
 
   const handleButton =(e)=>{
     e.preventDefault();
@@ -16,10 +19,28 @@ function App() {
         setIsPreviewShown(true) ;
   }
 
+  const handleSliderButton =(e)=>{
+    e.preventDefault();
+
+     if(isSlider)
+     setIsSlider(false)
+     else 
+     setIsSlider(true) ;
+  }
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  
+      <div className='col-md-12'>
+           
+           <div className='col-md-6'><button type="submit" onClick={handleButton}>SearchInput</button>
+           {isPreviewShown && <SearchInput/>}</div>
+           <div className='col-md-6'><button type="submit" onClick={handleSliderButton}>Slider</button>
+           {isSlider && <Slider/>}</div> 
+         </div>
+         {(!isSlider && !isPreviewShown) && <><img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code>  and save to reload.
         </p>
@@ -30,10 +51,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a></> }
+        
         <br></br>
-        <button type="submit" onClick={handleButton}>SearchInput</button>
-        {isPreviewShown && <SearchInput/>}
+      
        
       </header>
     </div>
